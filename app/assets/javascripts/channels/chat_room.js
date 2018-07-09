ff46ced93c3fdd506a85d25454cd0c7a805f5187
kazoo <kazuki_saito@diveintocode.jp>
2018-07-09 17:47:05 +0900

    build: channels created. chat function completed.

---
+++ app/assets/javascripts/channels/chat_room.js
@@ -0,0 +1,18 @@
+App.chat_room = App.cable.subscriptions.create("ChatRoomChannel", {
+  // connected: ->
+    // # Called when the subscription is ready for use on the server
+
+  // disconnected: ->
+    // # Called when the subscription has been terminated by the server
+
+  received: function(data) {
+    var chat = document.getElementById('chat');
+    var newMessage = document.createElement('p');
+    newMessage.innerText = data['message'];
+    chat.appendChild(newMessage);
+  },
+
+  speak: function(message) {
+    return this.perform('speak', { message: message });
+  }
+  });
