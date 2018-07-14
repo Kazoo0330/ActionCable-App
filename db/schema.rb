8f95bd8b15115161cfc029f37a9bda85f8ed2fc5
kazoo <kazuki_saito@diveintocode.jp>
2018-07-14 21:14:39 +0900

    build: models generated. user.rb battle.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,9 +1,19 @@
-ActiveRecord::Schema.define(version: 2018_07_09_032116) do
+ActiveRecord::Schema.define(version: 2018_07_14_120620) do
 
   enable_extension "plpgsql"
 
+  create_table "battles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "chat_messages", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
